f05c26e5d8175746f3631345d0e44832e3f488db
mohammed elabidi <mohammed.elabidi123@gmail.com>
2023-05-21 14:48:26 +0200

    zdd demande

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Demande;
 use App\Models\User;
 use Illuminate\Http\Request;
 
@@ -24,8 +25,8 @@ class HomeController extends Controller
      */
     public function index()
     {
-        $employers = User::count(); // 4 [4] , 
-        $demandes = 0;
+        $employers = User::count(); // 4 [4] ,
+        $demandes = Demande::where("status",null)->count();
         $soldes = 0;
 
         return view('home',compact("employers","demandes","soldes"));
